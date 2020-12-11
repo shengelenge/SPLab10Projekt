93019d4824de8f0d6336eb6c553a8126b2def0fb
Kuba <jakubsekula01@gmail.com>
2020-12-11 14:59:34 +0100

    gotowy program

--- plik.c
+++ plik.c
@@ -7,7 +7,22 @@ int main() {
 	int liczba; // szukana liczba
 		srand( time( NULL ) );
 		liczba = rand()%100;
-
+	
+	int x=-1; // liczba podana przez uzytkownika	
+	printf("Zgadnij liczbe\n");
+		do{
+      scanf("%d", &x);
+		  if (x>liczba) {
+		  printf("szukana liczba jest mniejsza\n");
+		  }
+		    if (x<liczba){
+		    printf("szukana liczba jest wienksza\n");	
+		    }
+		      if(x==liczba){
+		      printf("zgadles");
+          break;
+		      }
+		} while ( x !=liczba);
 return 0;
 
 }
