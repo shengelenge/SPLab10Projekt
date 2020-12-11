4a835226c99b6205d44ca74f53dec25d5d84bebd
Nicolas Wielocha <n.wielocha.114@studms.ug.edu.pl>
2020-12-11 14:46:50 +0100

    commit

--- plik.c
+++ plik.c
@@ -5,17 +5,7 @@
 int main() {
 
 	int liczba; // szukana liczba
-<<<<<<< HEAD
-
-	srand(time(NULL));
-	liczba = rand() % 101;
-
-}
-=======
-		srand(time(NULL));
-		liczba = rand() % 101;
 
 return 0;
 
 }
->>>>>>> origin/coBranch
